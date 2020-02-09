4cbcb7beb274adc1573a791f385828e37c4b1e02
tiendat161 <tiendat98vn@gmail.com>
2020-02-10 00:24:35 +0800

    Level-3 edit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,20 +5,26 @@ public class Duke {
         System.out.println(line + "    Hello I'm Dat\n" + "    What can I do for you?\n" + line);
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        String[] data = new String[100];
+        Task[] data = new Task[100];
         int a = 0;
         while (!input.equals("bye")) {
+            System.out.print(line);
+            String[] action = input.split(" ");
             if (input.equals("list")) {
-                System.out.print(line);
                 for (int i = 1; i <= a; i ++) {
                     System.out.println("    " + i + ". " + data[i]);
                 }
-                System.out.print(line);
+            } else if (action[0].equals("done")) {
+                int numOfTask = (int) Double.parseDouble(action[1]);
+                data[numOfTask].markDone();
+                System.out.println("    Nice! I've marked this task as done: ");
+                System.out.println("      " + data[numOfTask]);
             } else {
                 a++;
-                data[a] = input;
-                System.out.println(line + "    added: " + input + "\n" + line);
+                data[a] = new Task(input);
+                System.out.println("    added: " + data[a] + "\n");
             }
+            System.out.print(line);
             input = sc.nextLine();
         }
         System.out.println(line + "    Bye. Hope to see you again soon!\n" + line);

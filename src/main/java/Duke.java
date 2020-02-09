1084c1a9635b3200bc75e6117429158bd1215220
tiendat161 <tiendat98vn@gmail.com>
2020-02-09 23:42:29 +0800

    Level-2 edit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "    ____________________________________________________________\n";
-        System.out.println(line + logo + line);
-        System.out.println("    Hello I'm Dat\n" + "    What can I do for you?\n" + line);
+        System.out.println(line + "    Hello I'm Dat\n" + "    What can I do for you?\n" + line);
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        String[] data = new String[100];
+        int a = 0;
         while (!input.equals("bye")) {
-            System.out.print(line + "    " + input + "\n" + line);
+            if (input.equals("list")) {
+                System.out.print(line);
+                for (int i = 1; i <= a; i ++) {
+                    System.out.println("    " + i + ". " + data[i]);
+                }
+                System.out.print(line);
+            } else {
+                a++;
+                data[a] = input;
+                System.out.println(line + "    added: " + input + "\n" + line);
+            }
             input = sc.nextLine();
         }
         System.out.println(line + "    Bye. Hope to see you again soon!\n" + line);
